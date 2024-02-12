cce1a6d79f798336dcdbf8edcc57cbe2087b1dbe
bogle <bogleoneil1@gmail.com>
2024-02-12 05:38:39 -0500

    Working text to speach

---
+++ asr.py
@@ -0,0 +1,21 @@
+
+
+
+import speech_recognition as sr
+
+# create a recognizer object
+r = sr.Recognizer()
+
+# use the microphone as the audio source
+with sr.Microphone() as source:
+    print("Listening...")
+    audio = r.listen(source)
+
+try:
+    # use google's speech recognition to convert the audio to text
+    text = r.recognize_google(audio)
+    print("You said: " + text)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
