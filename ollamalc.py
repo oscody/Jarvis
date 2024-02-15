d1cf5e0558d99845bab320fedbf7c77bac1a57c3
bogle <bogleoneil1@gmail.com>
2024-02-14 21:02:55 -0500

    update ollama notes

--- ollamalc.py
+++ ollamalc.py
@@ -1,6 +1,7 @@
 # Testing Ollama running locally
 # With Ollama serve
 # Need to also run ollama pull $modelName in other terminal 
+# pip install langchain-community
 
 from langchain_community.llms import Ollama
 
@@ -8,7 +9,7 @@ model = "tinyllama"
 
 llm = Ollama(model=model)
 
-question = "Answer in 1 sentence or less what is the capital oj Jamaica?"
+question = "Answer in 1 sentence or less what is the capital of Jamaica the Island if you dont know the answer say idk?"
 
 response = llm.invoke(question)
 
