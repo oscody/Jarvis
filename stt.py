261aac6c33f56d297c4b076b1067d68331873127
bogle <bogleoneil1@gmail.com>
2024-02-14 19:30:44 -0500

    speach to text

---
+++ stt.py
@@ -0,0 +1,5 @@
+import pyttsx3
+
+engine = pyttsx3.init()  # Initialize the converter
+engine.say("Hello, world!")  # Add text to the speech queue
+engine.runAndWait()  # Process the speech queue and play the audio
