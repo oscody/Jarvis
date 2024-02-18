0c06125f32b4aa3994a1d1fccbbb676acaea140d
bogle <bogleoneil1@gmail.com>
2024-02-17 21:04:02 -0500

    Add speech output for "Thinking" in main function

--- jarvisWakeV2.py
+++ jarvisWakeV2.py
@@ -4,8 +4,14 @@ import pvporcupine
 import struct
 import pyaudio
 import pyttsx3
+import time
 from langchain_community.llms import Ollama
 import speech_recognition as sr
+from os import system
+import sys
+
+
+
 
 load_dotenv()
 
@@ -16,6 +22,26 @@ print(os.getenv('keyWordPath'))
 ACCESS_KEY = os.getenv('picovoice')
 
 KEYWORD_PATH = os.getenv('keyWordPath')
+
+# Initialize the Speech to Text engine
+engine = pyttsx3.init()
+
+
+# Create a recognizer object for Automatic Speech Recognition
+r = sr.Recognizer()
+source = sr.Microphone()
+
+
+
+
+def respond(text):
+    if sys.platform == 'darwin':
+        ALLOWED_CHARS = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,?!-_$:+-/ ")
+        clean_text = ''.join(c for c in text if c in ALLOWED_CHARS)
+        system(f"say '{clean_text}'")
+    else:
+        engine.say(text)
+        engine.runAndWait()
 
 def wake_word_detection():
     porcupine = None
@@ -59,28 +85,28 @@ def wake_word_detection():
         if porcupine is not None:
             porcupine.delete()
 
-def main():
+def think():
+
+
     # Initialize the Ollama model
     model = "tinyllama"
     llm = Ollama(model=model)
 
-    # Initialize the Speech to Text engine
-    engine = pyttsx3.init()
-
-    # Create a recognizer object for Automatic Speech Recognition
-    r = sr.Recognizer()
-
     while True:
         if wake_word_detection():
             # Use the microphone as the audio source
-            with sr.Microphone() as source:
+            with source as s:
                 print("Listening...")
-                audio = r.listen(source)
-
+                r.adjust_for_ambient_noise(s)
+                audio = r.listen(s)
+                
+                print("Listening............")
+                respond("Let me think about it.")
             try:
 
-                engine.say("Thinking")
-                engine.runAndWait()
+                # engine.say("Thinking")
+                # engine.runAndWait()
+                respond("Thinking")
 
                 # Use Google's speech recognition to convert the audio to text
                 text = r.recognize_google(audio)
@@ -95,13 +121,24 @@ def main():
                 
                 # Speak the text
                 print(f"Response is {response}")
-                engine.say(response)
-                engine.runAndWait()
+                # engine.say(response)
+                # engine.runAndWait()
+                respond(response)
+
 
             except sr.UnknownValueError:
                 print("Google Speech Recognition could not understand audio")
             except sr.RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+def main():
+
+    think()
+    # time.sleep(1)
+    # respond("Goodbye.")
+
+
 
 if __name__ == '__main__':
     main()
